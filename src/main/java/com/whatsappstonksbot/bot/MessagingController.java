f6a2a4706029c2692ac0183b1e6016af8a0e4949
Liz Moy <lmoy@c02zg1vllvdt.home>
2021-03-29 22:44:13 -0400

    Adding the rest of the code in the controller

--- src/main/java/com/whatsappstonksbot/bot/MessagingController.java
+++ src/main/java/com/whatsappstonksbot/bot/MessagingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @RestController
@@ -20,6 +21,25 @@ public class MessagingController {
             repeatCallers.add(fromNumber);
             return "Welcome to stonksbot \uD83D\uDCC8. Text me a valid ticker symbol and I'll give you quote data.";
         }
-        return "We've heard from you.";
+
+        Optional<Price> price = FinnhubService.getStockDetails(symbol);
+
+        if (price.isPresent()){
+            return messageBuilder(price.get(), symbol);
+        } else {
+            return "Sorry. We couldn't find any info on that one. Try another.";
+        }
+    }
+
+
+    private String messageBuilder(Price price, String symbol) {
+        return String.format("Here are the most up-to-date %s prices:\n" +
+                        "\uD83C\uDF05 Open price of the day: $%s\n" +
+                        "\uD83D\uDCC8 High price of the day: $%s\n" +
+                        "\uD83D\uDCC9 Low price of the day: $%s\n" +
+                        "\uD83D\uDD14 Current price: $%s\n" +
+                        "\uD83D\uDCC6 Previous close price: $%s\n" +
+                        "\uD83E\uDD1E Send another symbol.",
+                symbol.toUpperCase(), price.open, price.high, price.low, price.current, price.close);
     }
 }
